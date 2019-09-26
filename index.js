ea568105a9b0ab4df75fb1086c7917dbac680e55
JackRyan1989 <jack.jackryan@gmail.com>
2019-09-26 19:50:04 -0400

    TicketMaster API integrated.

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ $(document).ready(function () {
     var specCompURL = 'http://api.sportradar.us/ufc/trial/v2/en/competitors/sr:competitor:237660/summaries.json?api_key=ywt2ucxtf9drabwswbvx863g';
     //Specific competitor profile:
     var specCompProfURL = 'http://api.sportradar.us/ufc/trial/v2/en/competitors/sr:competitor:237660/profile.json?api_key=ywt2ucxtf9drabwswbvx863g';
-    
-    
+    //Stub Hub api key:
+    var stubHubApiKey = "T1DZyGIApA99dALmjSw4I5SAmfdwQoPO";
+    //Ticket Master api key:
+    var ticketMasterAPI  = "RDPrWYOojToRbPLsg0Ah8DnWO7cMXk10";
 
-    var queryURL = 'https://newsapi.org/v2/everything?' +
-        'q=Ultimate Fighting Championship&' +
-        'from=' + searchDate +
-        'sortBy=popularity&' +
-        'apiKey=111a9f85a73d4bce961da2f278698314';
-
-
-
+    $("#submitButton").on("click", function () {
+        event.preventDefault();
+        var city = $("#cityInput").val().trim();
+        var state = $("#stateInput").val().trim();
+        var keyWord = "ufc";
+        var tmURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword="+keyWord+"&countryCode=US&city="+city+"&state="+state+"&apikey="+ticketMasterAPI;
+        console.log(tmURL);
+        $.ajax({
+            type:"GET",
+            url: tmURL,
+            async:true,
+            dataType: "json",
+            success: function(json) {
+                        console.log(json);
+                        if (!(json._embedded)) {
+                        console.log("Search somewhere else");
+                        };
+                     },
+            error: function(xhr, status, err) {
+                        // This time, we do not end up here!
+                     }
+          });
+        });
 });
 
